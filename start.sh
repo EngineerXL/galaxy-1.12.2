15a191ed98f4dfa299fb7c558b83130c666a31a6
EngineerXl <8928632@gmail.com>
2021-01-06 14:19:49 +0300

    Добавил скрипт для запуска сервера

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MIN_MEMORY=2G
+MAX_MEMORY=8G
+SERVER_ARG=-d64 -server -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC -XX:+UnlockExperimentalVMOptions -XX:+UseParNewGC -XX:+ExplicitGCInvokesConcurrent -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:+UseAdaptiveGCBoundary
+SERVER_JAR=forge-1.12.2-14.23.5.2854.jar
+
+while true
+do
+	echo $(date +"%T") Minecraft started!
+	java -Xms{$MIN_MEMORY} -Xmx{$MAX_MEMORY} ${SERVER_ARG} -jar ${SERVER_JAR} nogui
+	echo $(date +"%T") Minecraft closed or crushed, restarting!
+	sleep 1s
+done
