254533d852cd43ba916f6d126c964caf2a1314bc
EngineerXL <engineerxl@yahoo.com>
2023-01-21 23:32:32 +0300

    Обновил Extra Planets, Forge, Galacticraft

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 MIN_MEMORY=2G
 MAX_MEMORY=8G
 SERVER_ARG=-d64 -server -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC -XX:+UnlockExperimentalVMOptions -XX:+UseParNewGC -XX:+ExplicitGCInvokesConcurrent -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:+UseAdaptiveGCBoundary
-SERVER_JAR=forge-1.12.2-14.23.5.2854.jar
+SERVER_JAR=forge-1.12.2-14.23.5.2859.jar
 
 while true
 do
@@ -12,4 +12,3 @@ do
 	echo $(date +"%T") Minecraft closed or crushed, restarting!
 	sleep 5s
 done
-
